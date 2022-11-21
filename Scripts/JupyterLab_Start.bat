eb8bd283e0b2c83832c0a25806ad3585b9a7702f
Brandon <brandoncasa8@gmail.com>
2022-11-20 19:34:44 -0500

    fix

--- Scripts/JupyterLab_Start.bat
+++ Scripts/JupyterLab_Start.bat
@@ -1,3 +1,3 @@
 docker exec cyclegan-pytorch jupyter-lab stop 3001
-docker exec -it --workdir /app/Projects/ cyclegan-pytorch jupyter-lab --no-browser --port=3001
+docker exec -it --workdir /app/pytorch-CycleGAN-and-pix2pix/ cyclegan-pytorch jupyter-lab --no-browser --port=3001
 PAUSE
