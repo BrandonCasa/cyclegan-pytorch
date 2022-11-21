69d4817da52e65166964d206f63aacae5364510f
Brandon <brandoncasa8@gmail.com>
2022-11-20 20:41:13 -0500

    fff

--- RunEnvironment.bat
+++ RunEnvironment.bat
@@ -1,2 +1,2 @@
-docker compose run --name "cyclegan-pytorch" --rm -it -p 3001:3001 app /bin/bash
+docker compose run --name "cyclegan-pytorch" --rm -it -p 3001:3001 -p 8097:8097 app /bin/bash
 PAUSE
